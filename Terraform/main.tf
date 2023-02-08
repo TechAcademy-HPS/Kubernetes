3624c48285bab61eacedb6b912fb5270ff8a5c5a
yoshithac8 <78433771+yoshithac8@users.noreply.github.com>
2023-02-08 21:21:02 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws"{
+   region = "ap-south-1"
+   access_key = "AKIAWJDJZ666JBJRYGND"
+   secret_key = "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"
+   }
+
+module "ec2" {
+    source = "C:/Users/10710592/Desktop/Terraform/JavaTest/Terraform/ec2"
+}
