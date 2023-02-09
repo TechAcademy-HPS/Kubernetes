0bfade052aa5675ec5da0f60c9f431a48d8e9076
yoshithac8 <78433771+yoshithac8@users.noreply.github.com>
2023-02-09 11:24:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+# Configure the AWS Provider
+#provider "aws"{
+#   region = "ap-south-1"
+ #  access_key = "AKIAWJDJZ666JBJRYGND"
+ #  secret_key = "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"
+  # }
+
+module "ec2" {
+    source = "/var/lib/jenkins/workspace/terraform/Terraform/ec2"
+}
