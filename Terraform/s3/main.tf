e8f7ada68a5e2fd022690ed6fabf2983c51bc511
yoshithac8 <78433771+yoshithac8@users.noreply.github.com>
2023-02-09 14:35:02 +0530

    Update main.tf

--- Terraform/s3/main.tf
+++ Terraform/s3/main.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "yoshithac8-s3" {
-  bucket = "yoshithac89"
-  acl    = "private"
+# resource "aws_s3_bucket" "yoshithac8-s3" {
+#   bucket = "yoshithac89"
+#   acl    = "private"
 
-  tags = {
-    Name        = "Yoshithabucket"
-    Environment = "feature"
-  }
-}
+#   tags = {
+#     Name        = "Yoshithabucket"
+#     Environment = "feature"
+#   }
+# }
