8d6a24134389a957ffb50f62042afae8b65b2b6d
yoshithac8 <78433771+yoshithac8@users.noreply.github.com>
2023-02-09 12:34:10 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,13 @@
-Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+#Configure the AWS Provider
 provider "aws"{
   region = "ap-south-1"
   access_key = "AKIAWJDJZ666JBJRYGND"
