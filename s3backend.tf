35a9f6ec82100c39422782e5658b1d6440768b84
yoshithac8 <78433771+yoshithac8@users.noreply.github.com>
2023-02-09 14:03:32 +0530

    Update s3backend.tf

--- s3backend.tf
+++ s3backend.tf
@@ -1,4 +1,19 @@
-resource "aws_vpc" "myvpc"{
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+#Configure the AWS Provider
+provider "aws"{
+  region = "ap-south-1"
+  access_key = "AKIAWJDJZ666JBJRYGND"
+  secret_key = "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"
+  }
+resource "aws_dynamodb_table" "terraform_statelock"{
    cidr_block = "10.0.0.0/16"
    tags = {
     Name = "MyVpc"
