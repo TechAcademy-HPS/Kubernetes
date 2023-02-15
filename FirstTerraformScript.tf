be8291bd24272a96c7b7172ece9bad2c72c9ee8e
yoshithac8 <78433771+yoshithac8@users.noreply.github.com>
2023-02-15 14:36:25 +0530

    Update FirstTerraformScript.tf

--- FirstTerraformScript.tf
+++ FirstTerraformScript.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "AWSEC2Instance" {
-   ami = "ami-0f9d9a251c1a44858"
+resource "aws_instance" "AWSEC2Instance"{
+   ami = "ami-0f9d9a251c1a44858"
    instance_type = "t2.micro"
    security_groups = ["launch-wizard-2"]
    key_name = "keypair"
@@ -11,4 +11,3 @@ resource "aws_instance" "AWSEC2Instance" {
     Name = "Redhat_Terraform"
     Environment = "dev"
    }
-  } 
