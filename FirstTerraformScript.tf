351e5e94ad54914cba25f10af8f240b40144f1d2
yoshithac8 <78433771+yoshithac8@users.noreply.github.com>
2023-02-20 16:50:42 +0530

    Update FirstTerraformScript.tf

--- FirstTerraformScript.tf
+++ FirstTerraformScript.tf
@@ -2,13 +2,37 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "AWSEC2Instance"{
-   ami = "ami-0f8ca728008ff5af4"
-   instance_type = "t2.micro"
-   security_groups = ["launch-wizard-2"]
-   key_name = "keypair"
-   tags = {
-    Name = "Ubuntu_Terraform"
-    Environment = "dev"
-   }
+resource "aws_instance" "slave" {
+#  count                  = "2"
+  ami                    = "ami-0f8ca728008ff5af4"
+  instance_type          = "t2.micro"
+  key_name               = "keypair"
+  vpc_security_group_ids = [aws_security_group.my-ec2-security.id]
+  tags = {
+     Name = "my-ec2-instance"
+  }  
+} 
+ resource "aws_security_group" "my-ec2-security" {
+  name = "my-ec2-security"   
+  ingress {
+    description = "Allow port 22 - inbound"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow port 8080 - inbound"
+    from_port   = 8080
+    to_port     = 8080 
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }   
+  egress {
+    description = "outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
